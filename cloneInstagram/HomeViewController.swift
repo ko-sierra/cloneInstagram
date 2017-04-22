f2f7d3cb140d1a821e5940c0d6a36a14d85a7901
清水光二 <koji22468234@icloud.com>
2017-04-22 09:01:15 +0900

    Initial Commit

---
+++ cloneInstagram/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  cloneInstagram
+//
+//  Created by 清水光二 on 2017/04/21.
+//  Copyright © 2017年 koji. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
